1ae7de278fa658ffc53243c60ab24ea4a8dc9e03
Dan Chiev <danchiev2@Gmail.com>
2020-09-16 00:05:58 -0400

    updated for loops

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
+
+
 
 // Arrays for characters
 var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -19,44 +21,52 @@ var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 var numericChar = ["1", "2","3", "4","5","6","7","8","9"];
 var specialChar = ["!","?","$","\&", "\\", "%"];
 
+// Prompts and Confirms for length, lowercase, upper case, numeric, and special characters
+
+var confirmLength = prompt ("How many characters do you want?");
+var confirmLower = confirm ("Do you want lower case characters?");
 var confirmUpper= confirm("Do you want upper case characters?");
 var confirmNumeric = confirm("Do you want numeric characters?");
 var confirmSpecial = confirm("Do you want special characters?"); 
 
 // for loop for lower case characters
 function generatePassword () {
+  if (confirmLength < 8 || confirmLength >=128 ){
+    return "Too many characters!"
+  }
   password=""
-  for (var i = 0; i <= 10; i++) {
+  if (confirmLower) {
+    for (var i = 0; i <= confirmLength; i++) {
 
-    password = password + lowerCaseChar[ (Math.floor(Math.random() * Math.floor(lowerCaseChar.length - 1))) ];
+      password = password + lowerCaseChar[ (Math.floor(Math.random() * Math.floor(lowerCaseChar.length - 1))) ];
+    }
   }
-  return password
-}
+  if (confirmUpper) {
+    // password = get first letter of password string and replace it with everything after + sign below. 
+      password = password + upperCaseChar[(Math.floor(Math.random() * Math.floor(upperCaseChar.length - 1)))];
 
-// If they want upper case characters
-if (confirmUpper) {
-  for (var u = 0; u <= 10; u++ ) {
-
+    
   }
-  password = password + upperCaseChar[(Math.floor(Math.random() * Math.floor(upperCaseChar.length - 1)))];
-  return password
-}
+  
+  // If they want numeric characters, this adds 2 to the password. 
+  if (confirmNumeric) {
+    // replace the second letter
 
-// If they want numeric characters
-if (confirmNumeric) {
-  for (var n = 0; n <= 10; n++ ) {
-
+      password = password + numericChar[(Math.floor(Math.random() * Math.floor(numericChar.length - 1)))];
+    
+    
+    
   }
-  password = password + numericChar[(Math.floor(Math.random() * Math.floor(numericChar.length - 1)))];
-  return password
-}
-
-// If they want special characters
-if (confirmSpecial) {
-  for (var s = 0; s <= 10; s++ ) {
-
+  
+  // If they want special characters, this adds 2 to the password. 
+  if (confirmSpecial) {
+    // replace the third letter 
+ 
+      password = password + specialChar[(Math.floor(Math.random() * Math.floor(specialChar.length - 1)))];
+    
+    
+    
   }
-  password = password + upperCaseChar[(Math.floor(Math.random() * Math.floor(upperCaseChar.length - 1)))];
   return password
 }
 
